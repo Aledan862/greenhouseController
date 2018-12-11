247ca56865135779384c1bd3a0dd96cdcd00ac67
Алексей Олегович Вахрушев <vakhrushev@technolink.ru>
2018-12-11 17:18:29 +0300

    add adafrut lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <Arduino.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #define ONE_WIRE_BUS 2
 #define PUBLISH_DELAY 3000
 #define SP_MAX 90
